69f59d9aec99914f0cd0f5bbfb7c904f9b38ea68
Farkass47 <sandor.farkas@t-online.hu>
2020-01-28 19:41:35 +0100

    Updates

---
+++ precache-manifest.32556b33a34fc0bde265a50d53a93250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d176e87a3ff9e597525e920925dcee9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04563541a99cee254d96",
+    "url": "/static/css/main.f9905ed5.chunk.css"
+  },
+  {
+    "revision": "50083d10085ad9fbe737",
+    "url": "/static/js/2.919d827c.chunk.js"
+  },
+  {
+    "revision": "67935b6d863b3e23bc08cf31dbc40ecc",
+    "url": "/static/js/2.919d827c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04563541a99cee254d96",
+    "url": "/static/js/main.9fad6c6b.chunk.js"
+  },
+  {
+    "revision": "6352734873d2e10388c3",
+    "url": "/static/js/runtime-main.580c046f.js"
+  }
+]);
